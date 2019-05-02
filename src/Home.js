0d3c1d52637265017ec04628d8e092f07b285a31
Teresa Cho <tcho@cca.edu>
2019-05-02 12:03:33 -0700

    fixed nav in Home and App.js, added Hero Splash

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
-// import Hero_Splash
+import Hero_Splash from './components/Landing/Hero_Splash'
 
 class Home extends Component {
     render() {
         return (
             <>
-            <Nav />
+            <Hero_Splash />
 
             </>
         )
