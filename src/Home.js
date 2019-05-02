d8effb3814c2d2554bd927a56a172824bdb2f0c0
Teresa Cho <tcho@cca.edu>
2019-05-02 11:54:23 -0700

    updated Nav and created landing components and assets WIP

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
+// import Hero_Splash
 
 class Home extends Component {
     render() {
         return (
             <>
             <Nav />
+
             </>
         )
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,16 @@ import React from 'react';
 const nav = () => {
     return (
         <div id="navbar">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Wayfarer</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Wayfarer</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">log in</a>
-                <a class="nav-item nav-link" href="#">sign up</a>
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                <a className="nav-item nav-link active" href="#">home <span className="sr-only">(current)</span></a>
+                <a className="nav-item nav-link" href="#">log in</a>
+                <a className="nav-item nav-link" href="#">sign up</a>
                 </div>
             </div>
             </nav>
