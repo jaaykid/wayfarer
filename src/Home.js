d6883f00d0097a46a47630a3005a7342c922c01d
Teresa Cho <tcho@cca.edu>
2019-05-02 10:58:38 -0700

    Created home.js and added nav to components

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+            <Nav />
+            </>
+        )
+    }
+}
+
+export default Home;
--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-// TODO: connect this when Home exists
-//import Home from '../components/Home';
+import Home from '../Home';
 import ProfilePosts from '../ProfilePosts';
 import CityPosts from '../CityPosts'
 
 export default (
   <Switch>
-    {/* <Route exact path="/" component={Home} /> */}
+    <Route path="/home" component={Home} />
     <Route path="/profile" component={ProfilePosts} />
     <Route path="/city" component={CityPosts} />
   </Switch>
